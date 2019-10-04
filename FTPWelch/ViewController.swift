dead92cb2109f446338ce8c33c017f043f225831
user914589 <user914589@74-80-250-216.ic2net.net>
2019-10-03 20:16:36 -0700

    first commit

---
+++ FTPWelch/ViewController.swift
@@ -0,0 +1,68 @@
+
+import UIKit
+import FilesProvider
+
+class ViewController: UIViewController, FileProviderDelegate {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+         let credentials = URLCredential(user: "sample", password: "sample", persistence: .forSession)
+               
+               let host = URL(string: "ftp://ftp.site.com")!
+               let ftpProvider = FTPFileProvider(baseURL: host, passive: false, credential: credentials)!
+
+        ftpProvider.copyItem(localFile:
+            URL(fileURLWithPath: "./test.csv"), to: "/directory/test.csv", overwrite: true, completionHandler: { (error) in
+                   print(error?.localizedDescription ?? "no error")
+               })
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func fileproviderSucceed(_ fileProvider: FileProviderOperations, operation: FileOperationType) {
+        switch operation {
+        case .copy(source: let source, destination: let dest):
+            print("\(source) copied to \(dest).")
+        case .remove(path: let path):
+            print("\(path) has been deleted.")
+        default:
+            if let destination = operation.destination {
+                print("\(operation.actionDescription) from \(operation.source) to \(destination) succeed.")
+            } else {
+                print("\(operation.actionDescription) on \(operation.source) succeed.")
+            }
+        }
+    }
+    
+    func fileproviderFailed(_ fileProvider: FileProviderOperations, operation: FileOperationType, error: Error) {
+        switch operation {
+        case .copy(source: let source, destination: let dest):
+            print("copying \(source) to \(dest) has been failed.")
+        case .remove:
+            print("file can't be deleted.")
+        default:
+            if let destination = operation.destination {
+                print("\(operation.actionDescription) from \(operation.source) to \(destination) failed.")
+            } else {
+                print("\(operation.actionDescription) on \(operation.source) failed.")
+            }
+        }
+    }
+    
+    func fileproviderProgress(_ fileProvider: FileProviderOperations, operation: FileOperationType, progress: Float) {
+        switch operation {
+        case .copy(source: let source, destination: let dest) where dest.hasPrefix("file://"):
+            print("Downloading \(source) to \((dest as NSString).lastPathComponent): \(progress * 100) completed.")
+        case .copy(source: let source, destination: let dest) where source.hasPrefix("file://"):
+            print("Uploading \((source as NSString).lastPathComponent) to \(dest): \(progress * 100) completed.")
+        case .copy(source: let source, destination: let dest):
+            print("Copy \(source) to \(dest): \(progress * 100) completed.")
+        default:
+            break
+        }
+    }
+}
---
+++ Pods/Target Support Files/Pods-FTPWelchUITests/Pods-FTPWelchUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FTPWelchUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FTPWelchUITests
+@end
